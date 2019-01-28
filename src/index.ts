3e24e2908bbcbcfdb89a2f45e5a0a5d192a01de7
nursh <nsheikhmohammed@yahoo.com>
2019-01-28 14:42:45 -0500

    Finished with logout

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ const main = async () => {
   const server = new ApolloServer({
     schema,
     formatError: formatArgumentValidationError,
-    context: ({ req }: any ) =>  ({ req })
+    context: ({ req, res }: any ) =>  ({ req, res })
   });
 
   const app = express();
